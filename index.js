456df91adf70549036981906573b2f8cde60d798
Wang-Zhongwei <873994270@qq.com>
2022-09-10 10:45:35 -0400

    configure body-parser

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 app.post("/api/fileanalyse", function(req, res) {
-  let upfile = req.files.upfile;
+  let upfile = req.body.upfile;
 
   // return a json response with the file name, type and size 
   res.json({
